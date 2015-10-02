f8532728c2a8ce8cb8512428d84735f13d7d2449
Jena Grafton <JenaLGrafton@gmail.com>
2015-10-02 16:22:57 -0500

    Added Navigation bar, toolbar with buttons, and code for stopwatch timer

--- Stopwatch/ViewController.swift
+++ Stopwatch/ViewController.swift
@@ -9,10 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var time = 0
+    
+    
+    func increaseTimer() {
+        time++
+        timerLabel.text = "\(time)"
+    }
+    
+    var timer = NSTimer()
 
+    
+    @IBOutlet var timerLabel: UILabel!
+    
+    @IBAction func startButton(sender: AnyObject) {
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTimer"), userInfo: nil, repeats: true)
+        
+    }
+    
+    @IBAction func pauseButton(sender: AnyObject) {
+        
+        timer.invalidate()
+    }
+    
+
+    @IBAction func resetButton(sender: AnyObject) {
+        
+        timer.invalidate()
+        
+        time = 0
+        
+        timerLabel.text = "0"
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
